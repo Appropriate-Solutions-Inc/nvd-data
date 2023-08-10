605895c20f18d1b39ec3f6c51c45dbc61771442a
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-10 12:54:41 -0400

    update-downloads ready for testing

--- update-downloads.py
+++ update-downloads.py
@@ -48,19 +48,14 @@ if api_key:
     api_sleep = 0.7
 
 
-def all_importables(conn: sqlite3.Connection) -> list[str]:
-    """Return sorted list of importables."""
-    cur = conn.cursor()
-    cur.execute("SELECT importable FROM meta;")
-    importables = [x[0] for x in cur.fetchall()]
-    importables.sort()
-    return importables
-
-
-def importables_to_check(conn: sqlite3.Connection) -> list[str]:
+def get_importables(
+    conn: sqlite3.Connection, sql: str, params: list = None
+) -> list[str]:
     """Return sorted list of importables where `needs_update` is 0."""
+    if params is None:
+        params = []
     cur = conn.cursor()
-    cur.execute("SELECT importable FROM meta WHERE needs_update=0;")
+    cur.execute(sql, params)
     importables = [x[0] for x in cur.fetchall()]
     importables.sort()
     return importables
@@ -112,24 +107,33 @@ def get_stored_modified_date(conn: sqlite3.Connection, name: str) -> datetime:
     return datetime.strptime(mod_date, "%Y-%m-%d %H:%M:%S%z")
 
 
-def set_needs_update(conn: sqlite3.Connection, importable: str):
-    """Set `needs_update` to 1 (true) in meta table for importable."""
+def set_modified_date(
+    conn: sqlite3.Connection, importable: str, last_modified_date: datetime
+):
+    """Update last_modified_date and set `needs_update` to 1 (true) in meta table."""
     cur = conn.cursor()
     cur.execute(
-        "UPDATE meta SET needs_update=1 WHERE importable=?;",
-        [importable],
+        "UPDATE meta SET needs_update=1, last_modified_date=? WHERE importable=?;",
+        [last_modified_date, importable],
     )
     conn.commit()
 
 
-def check_metadata(conn: sqlite3.Connection):
+def clear_needs_update(conn: sqlite3.Connection, importable: str) -> None:
+    """Clear `needs_update` flag for importable."""
+    cur = conn.cursor()
+    cur.execute("UPDATE meta SET needs_update=0 WHERE importable=?;", [importable])
+    conn.commit()
+
+
+def check_metadata(conn: sqlite3.Connection) -> list[str]:
     """Flag new metadata files.
 
     Compare meta file lastModifiedDate against database.
     Set `needs_update` to 1 (true) if date is newer.
     """
-    modified_count = 0
-    for importable in all_importables(conn):
+    modifieds = []
+    for importable in get_importables(conn, "SELECT importable FROM meta;"):
         name = f"{importable}.meta"
         path = data_dir / name
         print(f"Checking meta data for: {importable}.")
@@ -138,22 +142,42 @@ def check_metadata(conn: sqlite3.Connection):
 
         if file_date > db_date:
             print(f"{importable} needs update.")
-            set_needs_update(conn, importable)
-            modified_count += 1
+            set_modified_date(conn, importable, file_date)
+            modifieds.append(importable)
 
-    print(f"Number of modified files: {modified_count}.")
+    print(f"Number of modified files: {len(modifieds)}.")
+    return modifieds
+
+
+def download_cves(conn: sqlite3.Connection) -> None:
+    """Download the updated cve.json.gz files to the ./data directory."""
+    for importable in get_importables(conn, "SELECT * FROM meta WHERE needs_update=1"):
+        url = f"{base_url}/{importable}.json.gz"
+        print(f"Downloading: {url}.")
+        resp = download(url)
+        if resp.status_code != 200:
+            print(f"Failure code: {resp.status_code}")
+            continue
+
+        file_path = data_dir / f"{importable}.json.gz"
+        print(f"Storing: {file_path}.")
+        file_path.write_bytes(resp.content)
+        clear_needs_update(conn, importable)
 
 
 def main():
     conn = sqlite3.connect(db_path)
-    if not (importables := importables_to_check(conn)):
-        print("No importables need checking")
-        sys.exit(0)
-
-    print(f"Number of importables to process: {len(importables)}.")
-    # RGAC!! download_metas(importables)
+    # All importables that are not flagged for update.
+    # In practice this will(?) never be zero.
+    checkables = get_importables(
+        conn, "SELECT importable FROM meta WHERE needs_update=?;", [0]
+    )
+    print(f"Number of metas to check: {len(checkables)}.")
+    # RGAC!! download_metas(checkables)
 
     check_metadata(conn)
+    download_cves(conn)
+    print("Done")
 
 
 if __name__ == "__main__":
